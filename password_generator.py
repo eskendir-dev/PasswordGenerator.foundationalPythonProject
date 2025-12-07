3a7ce453f58831dd39c0c6c4399e5c6846a034bf
eskendir <eskendir2025@gmail.com>
2025-12-07 12:13:09 +0500

    Implement input validation for password length
    Added input validation for password length.

--- password_generator.py
+++ password_generator.py
@@ -2,10 +2,16 @@ import random
 
 symbols = 'abcdefghijklmnopqrstuvwxzABCDEFGHIJKLMNOPQRSTUVWXZ1234567890~`!@#$%^&*()_-+={[}]|\:;"?'
 password = ''
-length = int(input('Write length of password: \n')) 
+
+while True:
+    try:
+        length = int(input('Write length of password: \n')) 
+        break
+    except ValueError:
+        print("Error: Please enter a valid whole number. Try again.")
 
 for i in range(length): 
     random_symbol = random.choice(symbols)
     password += random_symbol
-        
+    
 print('Here is your password: ', password)
